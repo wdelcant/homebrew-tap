c31ea75e766e149b5626a78eb80b6818a6d75941
Wilson Del Canto <wilsondelcanto.redes@gmail.com>
2026-07-03 17:14:22 -0400

    Add invgate-cli v0.1.0

---
+++ invgate-cli.rb
@@ -0,0 +1,34 @@
+class InvgateCli < Formula
+  desc "Runtime OpenAPI/Swagger CLI — works with any Swagger/OpenAPI spec"
+  homepage "https://github.com/wdelcant/invgate-cli"
+  license "MIT"
+  version "0.1.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/wdelcant/invgate-cli/releases/download/v0.1.0/invgate-cli_0.1.0_macOS_arm64.tar.gz"
+      sha256 "SKIP"
+    else
+      url "https://github.com/wdelcant/invgate-cli/releases/download/v0.1.0/invgate-cli_0.1.0_macOS_amd64.tar.gz"
+      sha256 "SKIP"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/wdelcant/invgate-cli/releases/download/v0.1.0/invgate-cli_0.1.0_Linux_arm64.tar.gz"
+      sha256 "SKIP"
+    else
+      url "https://github.com/wdelcant/invgate-cli/releases/download/v0.1.0/invgate-cli_0.1.0_Linux_amd64.tar.gz"
+      sha256 "SKIP"
+    end
+  end
+
+  def install
+    bin.install "invgate-cli"
+  end
+
+  test do
+    system "#{bin}/invgate-cli", "--version"
+  end
+end
